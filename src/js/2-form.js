54c5a423ba9df5c8d1bd4cdd83030217b61e9e9d
Vale4ka33 <150661954+Vale4ka33@users.noreply.github.com>
2024-03-25 13:01:42 +0200

    hw js-9 done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,31 @@
-console.log("Form");
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea[name="message"]');
+const email = form.querySelector('input[name="email"]');
+
+const userInfoKey = "feedback-form-state";
+let currentState = {};
+
+form.addEventListener('input', onFormInput);
+
+let storageElem = JSON.parse(localStorage.getItem(userInfoKey));
+if (storageElem) {
+    email.value = storageElem.email || '';
+    textarea.value = storageElem.message || '';
+}
+
+function onFormInput(event) {
+    currentState = {
+        email: email.value,
+        message: textarea.value
+    };
+    localStorage.setItem(userInfoKey, JSON.stringify(currentState));
+    storageElem = JSON.parse(localStorage.getItem(userInfoKey)); // 
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    localStorage.removeItem(userInfoKey);
+    email.value = '';
+    textarea.value = '';
+    console.log(storageElem);
+});
